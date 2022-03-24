559895f8d4d9e476bbd4997771ca9e5b2c613a1a
asamad35 <samad.abdus3535@gmail.com>
2022-03-25 03:45:00 +0530

    fix redux logger

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -4,7 +4,7 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 
 const middlewares = [thunk];
-if (process.env.NODE_ENV !== "prod") {
+if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
